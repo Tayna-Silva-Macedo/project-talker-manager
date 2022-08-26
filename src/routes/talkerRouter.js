43af52845db6972d5766b797da3d3e019209acdf
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-08-26 15:45:42 -0300

    Requisito 2

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -6,8 +6,19 @@ const talkerRouter = express.Router();
 
 talkerRouter.get('/', async (req, res) => {
   const talkers = await readAndWrite.getAllTalkers();
-  
+
   return res.status(200).json(talkers);
+});
+
+talkerRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await readAndWrite.getTalkerById(Number(id));
+
+  if (talker) {
+    return res.status(200).json(talker);
+  }
+
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 module.exports = talkerRouter;
--- src/utils/readAndWrite.js
+++ src/utils/readAndWrite.js
@@ -4,6 +4,7 @@ const readTalkerFile = async () => {
   const path = 'src/talker.json';
   try {
     const contentFile = await fs.readFile(path, 'utf-8');
+
     return JSON.parse(contentFile);
   } catch (error) {
     return null;
@@ -12,9 +13,18 @@ const readTalkerFile = async () => {
 
 const getAllTalkers = async () => {
   const talkers = await readTalkerFile();
+
   return talkers;
+};
+
+const getTalkerById = async (id) => {
+  const talkers = await readTalkerFile();
+  const talkerSearch = talkers.find((talker) => talker.id === id);
+
+  return talkerSearch;
 };
 
 module.exports = {
   getAllTalkers,
+  getTalkerById,
 };
