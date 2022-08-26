ce40ff2fcedbfefe34a145ff79edc369745c75f3
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-08-26 16:20:20 -0300

    Requisito 4

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const generateToken = require('../utils/generateToken');
 
+const { emailValidation, passwordValidation } = require('../middlewares/validations');
+
 const loginRouter = express.Router();
 
-loginRouter.post('/', (req, res) => {
+loginRouter.post('/', emailValidation, passwordValidation, (req, res) => {
   const token = generateToken();
 
   return res.status(200).json({ token });
